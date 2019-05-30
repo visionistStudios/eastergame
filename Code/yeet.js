15eb10a376853a814565e54f47a1b6ac097d619d
Visionist <escaperecovery@gmail.com>
2019-05-30 11:52:14 -0700

    Create yeet.js

---
+++ Code/yeet.js
@@ -0,0 +1,9 @@
+function yeetText(t){
+var l = t.length;
+var nt = "";
+var char = "`1234567890-=qwertyuiop[]\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ûÿÖÜ¢£¥₧ƒáíóúñÑªº";
+for (var i = 0; i < l; i++){
+var r = Math.random()*char.length;
+nt+=char[r];
+}
+}
