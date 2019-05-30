f1798a76e09858f7a568bfc184966636ad71cb6f
Smiles <47442090+Fluffcakes@users.noreply.github.com>
2019-05-30 12:08:22 -0700

    Update effects.js

--- Code/effects.js
+++ Code/effects.js
@@ -3,8 +3,13 @@ var l = t.length;
 var nt = "";
 var char = "`1234567890-=qwertyuiop[]\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ûÿÖÜ¢£¥₧ƒáíóúñÑªº";
 for (var i = 0; i < l; i++){
-var r = Math.floor(Math.random()*char.length);
-nt+=char[r];
+  if (t[i]==" "){
+    nt+=" ";
+  }
+  else{
+    var r = Math.floor(Math.random()*char.length);
+    nt+=char[r];
+  }
 }
 return nt;
 }
